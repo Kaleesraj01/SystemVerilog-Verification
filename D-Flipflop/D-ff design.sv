d75828ec8b082957d3f2627722ce5a73def4ac41
Kaleesraj M <kalishraj01@gmail.com>
2025-12-29 16:44:32 +0530

    Create D-ff design.sv

---
+++ D-Flipflop/D-ff design.sv
@@ -0,0 +1,15 @@
+// Code your design here
+module dff (
+  input logic  clk, rst ,
+  input logic din ,
+  output logic q);
+  
+  always @(posedge clk or posedge rst ) begin 
+    if (rst)
+      q<=1'b0;
+    
+    else 
+      q<= din ;
+    
+  end 
+endmodule 
