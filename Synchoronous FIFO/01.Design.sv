cc76b1587ac794a9697563ce5aea7dad51c2ee7f
Kaleesraj M <kalishraj01@gmail.com>
2026-01-05 15:02:26 +0530

    Update 01.Design.sv

--- Synchoronous FIFO/01.Design.sv
+++ Synchoronous FIFO/01.Design.sv
@@ -5,7 +5,7 @@
 module fifo #(parameter width=8 , depth =8) 
   ( input logic clk , rst , wr_en , rd_en ,
    input logic [width-1:0]wr_data ,
-   output logic [width-1:0]rd_data ,
+   output wire [width-1:0]rd_data ,
    output logic full , empty );
   
   logic [width-1:0] mem [0:depth-1]; // mem allocation ;
@@ -31,14 +31,16 @@ module fifo #(parameter width=8 , depth =8)
   always@(posedge clk or posedge rst) 
     if (rst) begin
       rd_ptr <= 0 ;
-      rd_data<=0;
+      
 //       cnt1<=0;
     end 
   else if  (rd_en==1&& !empty) begin
-    rd_data <= mem[rd_ptr[fifo_depth_log-1:0]];
+  
     rd_ptr <= rd_ptr+1 ;
   end 
+   assign rd_data = mem[rd_ptr[fifo_depth_log-1:0]];
     
+  
   assign full = (({~wr_ptr[fifo_depth_log],wr_ptr[fifo_depth_log-1:0]})== (rd_ptr));
                                
   assign empty = (wr_ptr==rd_ptr);
